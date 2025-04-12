09e2fc5886456d608560d9b0e81f382cb900ac11
Developer-Shuvo <im.computer.engineeer@gmail.com>
2025-04-12 17:37:52 +0600

    Even Number find out

--- while-loop.js
+++ while-loop.js
@@ -1,5 +1,18 @@
-let num = 20;
-while (num <= 100) {
-    console.log(num);
-    num++;
+// let num = 20;
+// while (num <= 100) {
+//     console.log(num);
+//     num++;
+// }
+
+
+// **********Even Number find out 1 to 50*************
+
+let number = 1;
+while (number <= 50) {
+    console.log(number);
+    number++;
+
+    if (number % 2 === 0) {
+        console.log("Even Number is:" + number)
+    }
 }
