b6a4dce4a2e551e503dda0cd60ef94c40a5e151b
Developer-Shuvo <im.computer.engineeer@gmail.com>
2025-04-12 17:25:25 +0600

    While loop using

---
+++ for-of.js
@@ -0,0 +1,138 @@
+// *************Using num********************
+// const Students = [12, 34, 56, 78, 90];
+
+// for (const num of Students) {
+//     console.log(num);
+// }
+
+
+
+
+// ***********Using Num in string*******************
+// const shopping = ['Milk', 'Bread', 'Egg', 'Butter', 'Cheese'];
+
+// for (const num of shopping) {
+//     console.log(num);
+// }
+
+
+// ***********For loop*****************
+
+// const bike = ["RoyalEnfield", "R15 V3", "Suzuki", "Pulsar"];
+
+// for (const num of bike) {
+//     console.log(num);
+// }
+
+
+// ***************While Loop********************
+
+// let number = 50;
+
+// while (number <= 100) {
+//     console.log(number);
+//     number++;
+// }
+
+
+
+// ****************For loop*******************
+
+// let shoppingItems = ['Milk', 'Bread', 'Egg', 'Butter', 'Cheese'];
+
+// for (const num of shoppingItems) {
+//     console.log(num);
+// }
+
+
+
+// ***************while loop using sum of numbers****************
+
+// let amount = 200;
+// let total = 0;
+
+// while (amount <= 500) {
+//     console.log(total);
+//     total = total + amount;
+//     console.log("Moot" + total);
+//     amount++;
+// }
+
+
+// ************Even number using while loop and for loop also***************
+
+// let marks = 85;
+
+// while (marks <= 100) {
+//     console.log(marks);
+//     marks++;
+
+//     if (marks % 2 === 0) {
+//         console.log("Even Number is: " + marks);
+
+//     }
+
+// }
+
+
+// ****************Odd Number **************
+
+// let money = 423;
+
+// while (money <= 500) {
+//     console.log(money);
+//     money++;
+
+//     if (money % 2 === 1) {
+//         console.log("Odd number is :" + money);
+//     }
+// }
+
+
+
+// **************** i invest at least 6 hours in a day, print this 60 times*************
+
+// const hours = 6;
+// const days = 60;
+
+
+// for (let i = 1; i <= days; i++) {
+//     console.log('I invest at least 6 hours in a day');
+
+// }
+
+
+
+// ********* i invest atleast 8 hours in a day, print this 10 times*************
+
+// const hours = 8;
+// const days = 10;
+
+// for (let i = 1; i <= days; i++) {
+
+//     console.log("i code everyday")
+// }
+
+
+
+// ******************************************
+
+// let price = 2990;
+// let discount = 20;
+// // discount = 20%;
+
+// discountPrice = price * discount / 100;
+// console.log("Discount Price is: " + discountPrice);
+// console.log(payableAmount = price - discountPrice);
+
+
+
+// **************************************
+
+let price = 3500;
+let discount = 30;
+
+discountPrice = price * discount / 100;
+payableAmount = price - discountPrice;
+console.log("Discount Price is :" + discountPrice);
+console.log("payable Amount is :" + payableAmount);
