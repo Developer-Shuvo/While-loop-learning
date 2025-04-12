68257f0e7197e387a2930660c2a25d5d6d7c81a5
Developer-Shuvo <im.computer.engineeer@gmail.com>
2025-04-12 18:03:56 +0600

    some new things learn

--- while-loop.js
+++ while-loop.js
@@ -7,12 +7,44 @@
 
 // **********Even Number find out 1 to 50*************
 
-let number = 1;
-while (number <= 50) {
-    console.log(number);
-    number++;
+// let number = 1;
+// while (number <= 50) {
+//     console.log(number);
+//     number++;
 
-    if (number % 2 === 0) {
-        console.log("Even Number is:" + number)
-    }
+//     if (number % 2 === 0) {
+//         console.log("Even Number is:", number)
+//     }
+// }
+
+
+
+// ** Odd number testing***
+
+// let number = 10;
+// while (number <= 100) {
+//     console.log(number);
+//     number++;
+
+
+//     if (number % 2 === 1) {
+//         console.lod("odd number is:", number)
+//     }
+
+
+
+// *****For loop******
+
+// for (let i = 5; i <= 20; i++) {
+//     console.log(i);
+// }
+
+
+// for (let num = 20; num <= 50; num++) {
+//     console.log(num);
+// }
+
+
+for (let i = 1; i <= 20; i++) {
+    console.log(i);
 }
