56604048a1e898b3cbb8dd92f73d3a3bcaf22911
Developer-Shuvo <im.computer.engineeer@gmail.com>
2025-04-13 18:56:51 +0600

    Some new thing learning

--- decremental.js
+++ decremental.js
@@ -14,8 +14,26 @@
 
 
 // **************************************************
-let number = [10, 30, 15, 12, 32];
-console.log(number.length);
-console.log(number[4]);
-console.log(number[8]);
-console.log(typeof number);
+// let number = [10, 30, 15, 12, 32];
+// console.log(number.length);
+// console.log(number[4]);
+// console.log(number[8]);
+// console.log(typeof number);
+
+
+
+// ***************************************************
+
+let money = [50, 150, 3000, 900];
+let shopping = ['shoes', 'slide', 'panjabi'];
+let location = ['Dhaka', 'Chittagong', 'Khulna'];
+
+console.log(money[3]);
+console.log(shopping[2]);
+
+shopping[1] = 'sharee';
+console.log(shopping);
+
+
+money[3] = 1111;
+console.log(money);
