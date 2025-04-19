32b9ab6768631d0637789f1c2823aa5507b7f759
Developer-Shuvo <im.computer.engineeer@gmail.com>
2025-04-19 20:27:05 +0600

    double check

--- double-check.js
+++ double-check.js
@@ -1,44 +1,74 @@
-// ****Double check ******
+// **************************Double check **********************************
 
-let manList = ['Karim', 'Karim', 'Rahim', 'Salam', 'Rahim', 'Salam', 'Jamal', 'Shakib', 'Shakib'];
-let womanList = ['Sakina', 'Rokeya', 'Sakina', 'Fatema', 'Fatema', 'Jahanara', 'Shabana', 'Shirin'];
-let moneyAmount = [100, 100, 100, 1500, 1500, 150, 200, 300, 150, 400, 550, 550, 400];
+// let manList = ['Karim', 'Karim', 'Rahim', 'Salam', 'Rahim', 'Salam', 'Jamal', 'Shakib', 'Shakib'];
+// let womanList = ['Sakina', 'Rokeya', 'Sakina', 'Fatema', 'Fatema', 'Jahanara', 'Shabana', 'Shirin'];
+// let moneyAmount = [100, 100, 100, 1500, 1500, 150, 200, 300, 150, 400, 550, 550, 400];
 
-function doubleCheck(name) {
-    // console.log(name);
-    let nameHolder = [];
-    for (let item of name) {
-        if (nameHolder.includes(item) === false) {
-            nameHolder.push(item);
-        }
-    }
-    return nameHolder;
-}
+// function doubleCheck(name) {
+//     // console.log(name);
+//     let nameHolder = [];
+//     for (let item of name) {
+//         if (nameHolder.includes(item) === false) {
+//             nameHolder.push(item);
+//         }
+//     }
+//     return nameHolder;
+// }
 
-let uniqueList = doubleCheck(moneyAmount);
-console.log(uniqueList);
+// let uniqueList = doubleCheck(moneyAmount);
+// console.log(uniqueList);
 
 
 
 // ***********************************************************************
 
-let location = ['Dhaka', 'Dhaka', 'Chittagong', 'Chittagong', 'Khulna', 'Rajshahi', 'Khulna', 'Barisal', 'Sylhet', 'Rangpur', 'Sylhet',
-    'Mymensingh'
-];
+// let location = ['Dhaka', 'Dhaka', 'Chittagong', 'Chittagong', 'Khulna', 'Rajshahi', 'Khulna', 'Barisal', 'Sylhet', 'Rangpur', 'Sylhet',
+//     'Mymensingh'
+// ];
+// let bikes = ['Hero', 'Bajaj', 'Yamaha', 'Honda', 'Suzuki', 'TVS', 'Hero', 'Bajaj', 'Yamaha', 'Honda', 'Suzuki', 'TVS'];
 
-function doubleCheckLocation(location) {
+// function doubleCheckLocation(location) {
+//     let blank = [];
+//     for (let item of location) {
+//         if (blank.includes(item) === false) {
+//             blank.push(item);
+//         }
+//     }
+//     return blank;
+// }
+
+
+// let uniqueLocation = doubleCheckLocation(bikes);
+// console.log(uniqueLocation);
+
+
+
+
+
+// ***********************************************************************
+
+let products = ['shampo', 'soap', 'toothpaste', 'toothbrush', 'shampo', 'soap', 'toothpaste', 'toothbrush', 'shampo', 'soap', 'toothpaste', 'toothbrush'];
+
+let fruits = ['banana', 'apple', 'orange', 'banana', 'apple', 'orange', 'banana', 'apple', 'orange', 'banana', 'apple', 'orange'];
+
+
+
+function doubleCheckProducts(products) {
+
     let blank = [];
-    for (let item of location) {
+    for (let item of products) {
         if (blank.includes(item) === false) {
             blank.push(item);
         }
+
     }
     return blank;
+
+
+
 }
 
 
-
-
-
-let uniqueLocation = doubleCheckLocation(location);
-console.log(uniqueLocation);
+let differentProducts = doubleCheckProducts(fruits);
+console.log(differentProducts);
+console.log(differentProducts = doubleCheckProducts(products)); // different Console log ***
