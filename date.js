b048d6431247c27f94c5ae1c7080e6aa12658f93
Developer-Shuvo <im.computer.engineeer@gmail.com>
2025-04-20 14:41:18 +0600

    Date/month/year find out

---
+++ date.js
@@ -0,0 +1,6 @@
+let thisDay = new Date('2025-04-20');
+console.log("Find out the date =", thisDay.getDate()); // 20 
+
+console.log("find Out the month =", thisDay.getMonth());
+console.log("find out the year =", thisDay.getFullYear()); // 2025
+console.log("find out the day =", thisDay.getDay()); // 0 = Sunday, 1 = Monday, 2 = Tuesday, 3 = Wednesday, 4 = Thursday, 5 = Friday, 6 = Saturday
---
+++ math.js
@@ -0,0 +1,20 @@
+// *******************Minimum Number*******************
+
+// let lowest = Math.min(5, 10, 3, 25, 125, 333);
+// console.log('Minimum number of this array is : =', lowest);
+
+
+// *******************Maximum Number*******************
+// let highest = Math.max(222, 11, 122, 3212, 45, 77, 85, 39, 3444, 3333, 883478, 3244);
+// console.log('Maximum number of this array is : =', highest);
+
+
+// *******************PI is*******************
+// let Number = 12;
+// console.log("PI is the number of =", Math.PI * Number);
+
+// *******************difference between 2 number*******************
+// let num1 = 20;
+// let num2 = 5;
+
+// console.log('Defference betweeen 2 number is :=', Math.abs(num1 - num2));
